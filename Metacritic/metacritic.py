fb0790473095a9f9dc02909506d516b0079c271a
jiexy <ifechumek@gmail.com>
2023-03-22 19:03:46 -0400

    Change error log file to json

--- Metacritic/metacritic.py
+++ Metacritic/metacritic.py
@@ -9,7 +9,7 @@ movie_list = json.load(f)
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
-ERROR_FILE = "Metacritic/file_err_log.txt"
+ERROR_FILE = "Metacritic/file_err_log.json"
 
 
 def __get_param(movie_name: str):
