2046701fedfb6911b926822da8789a1c6bba4ce9
agnesrohra <61053622+agnesrohra@users.noreply.github.com>
2023-03-28 22:02:56 -0400

    Deleted existing json file

--- RottenTomatoes/movie_title_to_url.py
+++ RottenTomatoes/movie_title_to_url.py
@@ -1,11 +1,11 @@
 from requests import get
 import json
 from bs4 import BeautifulSoup
+import os
 
-with open("./movie_urls.json", "r") as urls:
-    url = json.load(urls)
+data = {}
 
-with open("../movie_titles_list.json", "r") as mlist:
+with open("./movie_titles_list.json", "r") as mlist:
     movies = json.load(mlist)
 
 for i in range(0, len(movies)):
@@ -14,13 +14,14 @@ for i in range(0, len(movies)):
     # search parameter
     search_url = "https://www.rottentomatoes.com/search?search=" + name.lower().replace(' ', '-')
     # the movie url
-    try:
-        search_results = get(search_url)
-        soup = BeautifulSoup(search_results.content, "html.parser")
-        movie_url = soup.find('search-page-media-row').find('a').attrs[0]
-        url[name] = movie_url
-    except Exception:
-        continue
+    search_results = get(search_url, headers=headers)
+    soup = BeautifulSoup(search_results.content, "html.parser")
+    movie_url = soup.find('search-page-media-row')
+    if movie_url != "None":
+        movie_url = movie_url.find('a').attrs['href']
+        data[name] = movie_url
+    else:
+        data[name] = ""
 
 with open("./RottenTomatoes/movie_urls.json", "w") as urls_list:
-    json.dump(url, urls_list)
+    json.dump(data, urls_list)
