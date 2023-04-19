b259231c4196259c3e6b3136a97cb72b95ab0e69
Shreyan Mallik <shrnmallik@gmail.com>
2023-04-18 21:55:11 -0400

    Stats for movies-release date

---
+++ imdb/stats/bar_line.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import json
+
+dictionary = json.load(open('./imdb/stats/movie_count_for_date.json', 'r'))
+xAxis = [key for key, value in dictionary.items()]
+yAxis = [value for key, value in dictionary.items()]
+plt.grid(True)
+
+## LINE GRAPH ##
+plt.plot(xAxis,yAxis, color='maroon', marker='o')
+plt.xlabel('variable')
+plt.ylabel('value')
+ax = plt.gca()
+ax.tick_params(axis='x', labelrotation = 90)
+
+## BAR GRAPH ##
+fig = plt.figure()
+plt.bar(xAxis,yAxis, color='maroon')
+plt.xlabel('variable')
+plt.ylabel('value')
+ax = plt.gca()
+ax.tick_params(axis='x', labelrotation = 90)
+plt.show()
