962ec4e234f0b418d365718409b5b2484310659c
jiexy <ifechumek@gmail.com>
2023-04-19 15:12:19 -0400

    Update the movie reviews with the sources

--- imdb/source_tag.py
+++ imdb/source_tag.py
@@ -2,22 +2,27 @@ import os
 import json
 from tqdm import tqdm
 
-for date_dir in os.listdir("./imdb/imdb_movie_jsons"):
-    new_dir = f"./imdb/imdb_movie_jsons/{date_dir}/"
-    try:
-        for each_movie in tqdm(os.listdir(new_dir)):
-            try:
-                file = f"./imdb/imdb_movie_jsons/{date_dir}/{each_movie}"
-                with open(file, "r+") as read_movie:
-                    load_curr_movie = json.load(read_movie)
-                    for review in load_curr_movie["reviews"]:
-                        review['source'] = 'imdb'
-                with open(file, "w") as write_to_movie:
-                    json.dump(load_curr_movie, write_to_movie, indent=4)
-                    # print(load_curr_movie)
-            except:
-                print("Skipping to next movie in dir")
-                continue
-    except:
-        print("Skipping to next dir")
-        continue
+def tag_reviews(dir_name, source):
+    for date_dir in os.listdir(dir_name):
+        new_dir = f"{dir_name}/{date_dir}/"
+        # print(new_dir)
+        # break
+        try:
+            for each_movie in tqdm(os.listdir(new_dir)):
+                try:
+                    movie_file = f"{new_dir}/{each_movie}"
+                    with open(movie_file, "r+") as read_movie:
+                        load_curr_movie = json.load(read_movie)
+                        for review in load_curr_movie["reviews"]:
+                            review['source'] = source
+                    with open(movie_file, "w", encoding="utf-8") as dataset_json:
+                        json.dump(load_curr_movie, dataset_json,  indent=2, ensure_ascii=False)
+                except:
+                    print("Skipping to next movie in dir ", each_movie)
+                    continue
+        except Exception:
+            print("Skipping to next dir ", dir_name)
+            continue
+
+if __name__ == "__main__":
+    tag_reviews("Metacritic/metacritic_movies_json", "metacritic")
