39f4b7f272111dd6a29102877a645cd84dec45b2
jiexy <ifechumek@gmail.com>
2023-04-26 16:47:13 -0400

    Remove Rubbish Reviews

--- imdb/lang_tag.py
+++ imdb/lang_tag.py
@@ -3,36 +3,47 @@ import os
 import json
 from tqdm import tqdm
 
-for date_dir in os.listdir("./imdb/imdb_movie_jsons"):
-    new_dir = f"./imdb/imdb_movie_jsons/{date_dir}/"
-    if int(date_dir) != 2018:
-        continue
+trash = []
+for date_dir in os.listdir("Metacritic/metacritic_movies_json"):
+    new_dir = f"Metacritic/metacritic_movies_json/{date_dir}/"
+    # if int(date_dir) != 2018:
+    #     continue
     try:
         for each_movie in tqdm(os.listdir(new_dir)):
             try:
-                file = f"./imdb/imdb_movie_jsons/{date_dir}/{each_movie}"
+                file = f"Metacritic/metacritic_movies_json/{date_dir}/{each_movie}"
 
                 with open(file, "r+") as read_movie:
                     load_curr_movie = json.load(read_movie)
                     for review in load_curr_movie["reviews"]:
                         content = review["content"]
-                        lang_prob = detect_langs(content)
-                        languages = [x.lang for x in lang_prob if x.prob > 0.7]
-                        if len(languages) > 0 and languages[0] == "en":
-                            review["lang"] = languages[0]
+                        try:
+                            lang_prob = detect_langs(content)
+                            languages = [x.lang for x in lang_prob if x.prob > 0.7]
+                            if len(languages) > 0 and languages[0] == "en":
+                                review["lang"] = languages[0]
+                        except:
+                            trash.append(content + "\n")
+                            review["lang"] = None
                         else:
                             continue
                     load_curr_movie["reviews"] = [
                         review
                         for review in load_curr_movie["reviews"]
-                        if review["lang"] == "en"
+                        if review.get("lang") == "en"
                     ]
                 with open(file, "w") as write_to_movie:
                     json.dump(load_curr_movie, write_to_movie, indent=4)
                     # print(load_curr_movie)
+                
             except:
+                print(file)
                 print("Skipping to next movie in dir")
                 continue
     except:
+        print(each_movie)
         print("Skipping to next dir")
         continue
+
+with open("./Rubbish_reviews.txt", 'a') as rubbish:
+    rubbish.write("".join(trash))
