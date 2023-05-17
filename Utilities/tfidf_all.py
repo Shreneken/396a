cd91c9a7145a27bf65b0c9b9d7f90c026ebefd9c
jiexy <ifechumek@gmail.com>
2023-05-16 22:52:30 -0400

    ..

--- Utilities/tfidf_all.py
+++ Utilities/tfidf_all.py
@@ -91,7 +91,7 @@ for movie_year in tqdm(tuple(os.walk("./MergedData/merged_movie_jsons"))[0][1]):
         mean = df["TF-IDF"].mean()
         print(mean - med)
         d = {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
-        file['vibes'] = [k for k,v in d.items() if abs(med - v) <= abs(mean - med)]
+        file['vibes'] = [k for k,v in d.items() if abs(med - v) <= 0]
         with open(f"{subdir[0][0]}/{file_name}", 'w', encoding="utf-8") as wr:
             json.dump(file, wr, indent=4)
-        cnt += 1; print(f"\rvar: {var} :: {cnt}/1205 :: {file['vibes']}", end="")
+        cnt += 1; print(f"\r{cnt}/1205 :: {file['vibes']}", end="")
